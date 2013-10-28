b020058b9491a33f20c91d0b2b61f57987f8e2cb
montana88 <Toni@063.221.028.145.hva.nl>
2013-10-28 10:49:26 +0100

    deeltoets 2

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,136 +1,67 @@
 // we maken hier onze eigen werkruimte om ervoor te zorgen 
 // dat onze code niet zal conflecteren met andere codes.
-// is een opject of een leef object.
-// Het voordeel van het gebruik van namespaces is dat functienamen niet kunnen conflicteren. Als veel programmeurs aan eenzelfde project werken
+// is een opject of een leeg object.
 var CONTENT = CONTENT || {};
 
+// om conflicten te voorkomen
 // self invoking anonymous function, Het belangrijkste hieraan is dat je Local scopes kan maken.
 // wanneer de functie runt wanneer het defined is dat je hierbij geen naam hoeft te geven aan deze functie
 (function () {
 	// Data objecten en zetten hier onze content in. voordeel aan OOP is de referentie naar real-life. lamp: property, height width; method, licht een actie.
 	CONTENT.game = {
-		title:'Pool A - Score: Boomsquad vs. Burning Snow',
-		items: [
-			{
-				description: 'Team'
-			}, {
-				description: 'Result'
-			}, {
-				description: 'Team'
-			}
-		],
-		items2: [
-			{
-				description: 'Boomsquad'
-			}, {
-				description: '15 - 8'
-			}, {
-				description: 'Burning Snow'
-			}
-		],
-		thead: [
-			{
-				description: 'Score'
-			}, {
-				description: 'Team'
-			}, {
-				description: 'Points'
-			}, {
-				description: 'Team'
-			}
-		],
-		tbody: [
-		    { score: "1", team1: "Boomsquad", team1Score: "1", team2: "Burning Snow", team2Score: "0"},
-		    { score: "2", team1: "Boomsquad", team1Score: "2", team2: "Burning Snow", team2Score: "0"},
-		    { score: "3", team1: "Boomsquad", team1Score: "2", team2: "Burning Snow", team2Score: "1"},
-		    { score: "4", team1: "Boomsquad", team1Score: "2", team2: "Burning Snow", team2Score: "2"},
-		    { score: "5", team1: "Boomsquad", team1Score: "3", team2: "Burning Snow", team2Score: "2"},
-		    { score: "6", team1: "Boomsquad", team1Score: "4", team2: "Burning Snow", team2Score: "2"},
-		    { score: "7", team1: "Boomsquad", team1Score: "5", team2: "Burning Snow", team2Score: "2"},
-		    { score: "8", team1: "Boomsquad", team1Score: "5", team2: "Burning Snow", team2Score: "3"},
-		    { score: "9", team1: "Boomsquad", team1Score: "6", team2: "Burning Snow", team2Score: "3"},
-		    { score: "10", team1: "Boomsquad", team1Score: "7", team2: "Burning Snow", team2Score: "3"},
-		    { score: "11", team1: "Boomsquad", team1Score: "7", team2: "Burning Snow", team2Score: "4"},
-		    { score: "12", team1: "Boomsquad", team1Score: "8", team2: "Burning Snow", team2Score: "4"},
-		    { score: "13", team1: "Boomsquad", team1Score: "8", team2: "Burning Snow", team2Score: "5"},
-		    { score: "14", team1: "Boomsquad", team1Score: "8", team2: "Burning Snow", team2Score: "6"},
-		    { score: "15", team1: "Boomsquad", team1Score: "9", team2: "Burning Snow", team2Score: "6"},
-		    { score: "16", team1: "Boomsquad", team1Score: "9", team2: "Burning Snow", team2Score: "7"},
-		    { score: "17", team1: "Boomsquad", team1Score: "10", team2: "Burning Snow", team2Score: "7"},
-		    { score: "18", team1: "Boomsquad", team1Score: "11", team2: "Burning Snow", team2Score: "7"},
-		    { score: "19", team1: "Boomsquad", team1Score: "12", team2: "Burning Snow", team2Score: "7"},
-		    { score: "20", team1: "Boomsquad", team1Score: "13", team2: "Burning Snow", team2Score: "7"},
-		    { score: "21", team1: "Boomsquad", team1Score: "14", team2: "Burning Snow", team2Score: "7"},
-		    { score: "22", team1: "Boomsquad", team1Score: "14", team2: "Burning Snow", team2Score: "8"},
-		    { score: "23", team1: "Boomsquad", team1Score: "15", team2: "Burning Snow", team2Score: "8"}
-    	]
+		
 	};
 
 	CONTENT.schedule = {
-		title:'Pool A - Schedule',
-		thead: [
-			{
-				description: 'Monday 18 March'
-			}, {
-				description: 'Team'
-			}, {
-				description: 'Result'
-			}, {
-				description: 'Team'
-			}
-		],
-		tbody: [
-		    { date: "Monday, 9:00am", team1: "Chasing", team1Score: "13", team2: "Amsterdam Money Gang", team2Score: "9"},
-		    { date: "Monday, 9:00am", team1: "Boomsquad", team1Score: "15", team2: "Beast Amsterdam", team2Score: "11"},
-		    { date: "Monday, 10:00am", team1: "Beast Amsterdam", team1Score: "14", team2: "Amsterdam Money Gang", team2Score: "12"},
-		    { date: "Monday, 10:00am", team1: "Chasing", team1Score: "5", team2: "Burning Snow", team2Score: "15"},
-		    { date: "Monday, 11:00am", team1: "Boomsquad", team1Score: "11", team2: "Amsterdam Money Gang", team2Score: "15"},    
-		    { date: "Monday, 11:00am", team1: "Burning Snow", team1Score: "15", team2: "Beast Amsterdam", team2Score: "6"},
-		    { date: "Monday, 12:00pm", team1: "Chasing", team1Score: "8", team2: "Beast Amsterdam", team2Score: "15"},
-		    { date: "Monday, 12:00pm", team1: "Boomsquad", team1Score: "15", team2: "Burning Snow", team2Score: "8"},
-		    { date: "Monday, 1:00pm", team1: "Chasing", team1Score: "15", team2: "Boomsquad", team2Score: "14"},
-		    { date: "Monday, 1:00pm", team1: "Burning Snow", team1Score: "15", team2: "Amsterdam Money Gang", team2Score: "11"}
-		]
+		
 	};
 
 	CONTENT.Ranking = {
-		title:'Pool A - Ranking',
-		thead: [
-			{
-				description: 'Team'
-			}, {
-				description: 'W'
-			}, {
-				description: 'L'
-			}, {
-				description: 'Points won'
-			}, {
-				description: 'Points lost'
-			}, {
-				description: '+/-'
-			}
-		],
-		tbody: [
-		    { team: "Chasing", Win: "2", Lost: "2", Sw: "7", Sl: "9", Pw: "35", Pl: "39"},
-		    { team: "Boomsquad", Win: "2", Lost: "2", Sw: "9", Sl: "8", Pw: "36", Pl: "34"},
-		    { team: "Burning Snow", Win: "3", Lost: "1", Sw: "11", Sl: "4", Pw: "36", Pl: "23"},
-		    { team: "Beast Amsterdam", Win: "2", Lost: "2", Sw: "6", Sl: "8", Pw: "30", Pl: "34"},
-		    { team: "Amsterdam Money Gang", Win: "1", Lost: "3", Sw: "6", Sl: "10", Pw: "30", Pl: "37"}
-		]
+		
 	};
 	
-	// Controller Init
+	// best practice
 	// geeft je een doorstart
+	// literal vs constructor = literal is gelijk al een variabelen
+	// constructor = officieel een function, function kan inladen in var
 	CONTENT.controller = {
 		init: function () {
+			console.log("controller.init ", CONTENT.controller);
+
+			// Set variables
+			var type 		=  'POST',
+				url  		=  'https://api.leaguevine.com/v1/game_scores/?tournament_id=19389&access_token=82996312dc',
+				postData 	= JSON.stringify({
+					game_id: '127236',
+				    team_1_score: '2',
+				    team_2_score: '4',
+				    is_final: 'False'
+				});
+
+			// Create request
+			var xhr = new XMLHttpRequest();
+			
+			// Open request
+			xhr.open(type,url,true);
+
+			// Set request headers
+			xhr.setRequestHeader('Content-type','application/json');
+			xhr.setRequestHeader('Authorization','bearer 82996312dc');
+			
+			// Send request (with data as a json string)
+			xhr.send(postData);
+
 			// Initialize router
 			CONTENT.router.init();
+			
 		}
 	};
 
 	// Router
 	CONTENT.router = {
 		init: function () {
+			console.log("router.init ", CONTENT.router);
+			//checked je URL.
 	  		routie({
 			    '/game': function() {
 			    	CONTENT.page.game();
@@ -142,6 +73,7 @@ var CONTENT = CONTENT || {};
 			    '/Ranking': function() {
 			    	CONTENT.page.Ranking();
 			    },
+			    
 			    '*': function() {
 			    	CONTENT.page.game();
 			    }
@@ -149,6 +81,7 @@ var CONTENT = CONTENT || {};
 		},
 
 		change: function () {
+			//qwery selector engine, selecteerd elementen uit DOM
             var route = window.location.hash.slice(2),
                 sections = qwery('section[data-route]'),
                 section = qwery('[data-route=' + route + ']')[0];  
@@ -165,33 +98,47 @@ var CONTENT = CONTENT || {};
             if (!route) {
             	sections[0].classList.add('active');
             }
-
 		}
 	};
 
 	// Pages
-	// zorgt ervoor wat waar in moet, de content van game komt dan op de gamepagina
+	// qwery selecteert een element uit DOM
+	// Transparency.render(juisteElement, data);
 	CONTENT.page = {
 		game: function () {
+			console.log("page.game " + CONTENT.page);
 			Transparency.render(qwery('[data-route=game')[0], CONTENT.game);
 			CONTENT.router.change();
 		},
-
 		schedule: function () {
-			Transparency.render(qwery('[data-route=schedule')[0], CONTENT.schedule);
+			console.log("content.shecule.tijden " + CONTENT.schedule.tijden);
+			schemaData: $$.json('https://api.leaguevine.com/v1/game_scores/?tournament_id=19389&access_token=4675792b56',{}, function(data){
+				
+				Transparency.render(qwery('[data-route=schedule]')[0], data);
+			})
 			CONTENT.router.change();
-		},
 
+		},
 		Ranking: function () {
-			Transparency.render(qwery('[data-route=Ranking')[0], CONTENT.Ranking);
+			console.log("CONTENT.Ranking.pools" + CONTENT.Ranking.pools);
+			poolData: $$.json('https://api.leaguevine.com/v1/pools/?tournament_id=19389&access_token=e6c8dbf9f2',{}, function(data){
+				
+				Transparency.render(qwery('[data-route=Ranking]')[0], data);
+			})
 			CONTENT.router.change();
 		}
 	}
-	// DOM ready
-	//wanneer de DOM opgebouwd is, word JavaScript pas gestart
+	
 	domready(function () {
 		// Kickstart application
 		CONTENT.controller.init();
 	});
 	
 })();
+
+//object literal configureren object of globale eenlingen
+//constructor maakt functie creeren van meerdere instanties
+//closure iets van een inner function een outer function kan benaderen
+//When we tell a program what order to do things in, we're using something called control flow. You already know about 'if' statements, but in this course, we'll expand our knowledge!
+	
+	
